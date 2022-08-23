f88d4bfa3cb99cecd156e8a805f3cde80366e90d
unknown <vikalpdwivedi9450@gmail.com>
2022-08-23 22:41:16 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import {Template} from './Components/Template';
+
+function App() {
+  return (
+    <div className="App">
+    <Template />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Template.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Images/Image 2.jpg'
+
+export const Template = () => {
+  return (
+  
+      <img src="./Images/Image 2.jpg" alt="Imag" />
+  
+  )
+}
